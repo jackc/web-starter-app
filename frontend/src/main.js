db09ce23faad07df0ae15ac66e468b1eb37abeb7
Jack Christensen <jack@jackchristensen.com>
2024-06-29 22:19:41 -0500

    Add Vite
    Auto reload on server change.

---
+++ frontend/src/main.js
@@ -0,0 +1,4 @@
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
+
+console.log("hello from main.js")
---
+++ frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite"
+import FullReload from "vite-plugin-full-reload"
+
+export default defineConfig({
+  root: "src",
+  base: "/assets",
+  server: {
+    port: 8080,
+    strictPort: true,
+    proxy: {
+      "^/(?!(assets))" : {
+        target: "http://localhost:8081"
+      }
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/js/main.js",
+    },
+  },
+  plugins: [
+    // Delay 100ms to allow the server to restart before triggering the page reload. Adjust as needed.
+    FullReload("../bin/web-starter-app", {delay: 100})
+  ]
+})
